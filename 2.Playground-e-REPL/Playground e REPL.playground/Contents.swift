51634dde5e406a703b482ba6a3570f073e8716b6
etnatraining <acaland@Antonios-Mac-mini.local>
2016-03-25 12:48:56 +0100

    renamed article 2

---
+++ 2.Playground-e-REPL/Playground e REPL.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+for x in 0..<10 {
+    x * x
+}
+
+
+
+var blue = UIColor.blueColor()
+var red = UIColor.redColor()
+
+var lbl = UILabel(frame: CGRectMake(0, 0, 100, 30))
+lbl.text = "Ciao a tutti"
+lbl.textColor = blue
+
+var img = UIImage(named: "swift-hero.png")
+
+var iv = UIImageView(image: img)
+
+
+
+
+
